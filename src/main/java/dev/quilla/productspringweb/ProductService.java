a499d45da96fc5e3eaad4a2a47c5e4dee1140793
quill-a <gabesander@gmail.com>
2025-08-17 01:30:16 -0500

    init commit

---
+++ src/main/java/dev/quilla/productspringweb/ProductService.java
@@ -0,0 +1,47 @@
+package dev.quilla.productspringweb;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ProductService {
+
+    @Autowired
+    ProductDB db;
+
+//    public void addProduct(Product p) {
+//
+//        db.save(p);
+//    }
+//
+    public List<Product> getAllProducts() {
+        return db.findAll();
+    }
+//
+//    public Product getProduct(String name) {
+//        for(Product p : products) {
+//            if(p.getName().equals(name)) return p;
+//        }
+//
+//        return null;
+//    }
+
+//    public List<Product> getProductWithText(String text) {
+//
+//        String str = text.toLowerCase();
+//        List<Product> products = new ArrayList<>();
+//
+//        for(Product p : products) {
+//
+//            String name = p.getName().toLowerCase();
+//            String type = p.getType().toLowerCase();
+//            String place = p.getPlace().toLowerCase();
+//
+//            if(name.contains(str) || type.contains(str) || place.contains(str)) products.add(p);
+//        }
+//
+//        return products;
+//    }
+}
---
+++ src/test/java/dev/quilla/productspringweb/ProductSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.quilla.productspringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
